02c60c4d199ff23c1b2b6d4d6893d101f8e00964
faizan542 <faizansilat100@gmail.com>
2021-03-30 16:21:16 +0500

    assignment complete

---
+++ app.js
@@ -0,0 +1,108 @@
+// Assignment 2 Start
+
+// Task 1: OBJECT.ASSIGN
+// let course = {
+//     name : "web programming",
+//     score : 92,
+// }
+
+// let anotherCourse = {
+//     name : "mobile app development",
+//     score : 85,
+// }
+
+// let finalResult = Object.assign(course, anotherCourse,{grade : "A+"})
+// console.log(finalResult)
+
+// TASK 2: NESTED TERNARY OPERATOR
+
+// let student = 16
+// let Adult;
+// let price;
+// let finalResult = (student <= 18) ? (price = 20) : (Adult) ? (price = 30):(price = 30);
+// console.log(finalResult)
+
+// TASK:3 FOR OF LOOP
+// const names = ["Youtube","Facebook","Instagram","Netflix","Whatsapp"]
+// for(const i of names){
+//     console.log(i)
+// }
+
+// TASK 3 FOR IN LOOP
+// const symbols = {
+//     yt : "Youtube",
+//     fb : "Facebook",
+//     ig : "Instagram",
+//     wtsapp : "Whatsapp"
+// };
+// for (const a in symbols){
+    // console.log(`key is : ${a} and value is ${symbols[a]}`)
+    // console.log("key is :" + a  + " and value is :" + symbols[a])
+// }
+
+// TASK 4: ARROW FUNCTION
+// Creating a regular function
+// const abc = function (){
+//     console.log("Hello World")
+// }
+// abc()
+
+// Converting it to an arrow function
+// const abc = ()=>{
+//     console.log("Hello World")
+// }
+// abc();
+// var  a = 3
+// var b = a++ + --a - a + a++ + ++a
+// console.log(b)
+
+// Assignment 2 END
+
+
+
+// Assignment 3 Start
+
+// Task 1: unique, sort an array in ascending order
+// let val =  [1,40,2,10,10,40,2,2,5,3,54,2,42,5,5,5]
+// console.log(val)
+// val.sort()
+// console.log(val)
+
+
+// Task 2: Print values using for loop in the following sequence
+
+// let i;
+// let a;
+// for(i=1 ; i <= 5 ; i++){
+//     for(a=1 ; a <= i ; a++){
+//         document.write(i + "&nbsp &nbsp")
+//     }{
+//         document.write("<br> <br>")
+//     }
+// }
+
+// for(i=1 ; i <= 5 ; i++){
+//     for(a=1 ; a <= i ; a++){
+//         document.write(a + "&nbsp &nbsp")
+//     }{
+//         document.write("<br> <br>")
+//     }
+// }
+
+// for(i=1 ; i <= 5 ; i++){
+//     for(a=1 ; a <= i ; a++){
+//         document.write("😎" + "&nbsp &nbsp")
+//     }{
+//         document.write("<br> <br>")
+//     }
+// }
+
+// Task 3:  Sum the folliwg number using for loop
+// let arr = [1,2,3,4,5]
+// let sum = 0
+// for(i = 0 ; i < arr.length; i++){
+//     sum = sum +arr[i]
+// }
+// document.write(sum)
+
+
